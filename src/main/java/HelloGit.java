774100588300f942a2531475f49db3883e0da14a
qiaoguohui <buffer@yeah.net>
2021-01-11 15:47:03 +0800

    更新HelloGit程序文件

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,5 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello git!");
+        String str = "Hello git!";
+        System.out.println(str);
     }
 }
